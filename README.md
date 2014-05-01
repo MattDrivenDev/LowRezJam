19afdaabbe905570df0df232fb539c52d0141b9c
Matt Ball <saxonmatt@gmail.com>
2014-05-01 13:39:07 +0100

    First code commit - the basics for rendering a game to the screen are in place.

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # lowrezjam
 
 Having a crack at [LowRezJam 2014](http://jams.gamejolt.io/lowrezjam2014) using [MonoGame](http://www.monogame.net/) and [F#](http://fsharp.org/).
+
+## Notes
+
+* The game is built at a resolution of `32x32` pixels - which I'm scaling up by 20 so that the actual view area is now `640x640`.
+* The **only** graphical asset is the `pixel.xnb` which is a `1x1` grey png file compiled to the XNA/MonoGame 2D texture filetype. This get's scaled up when drawn to the screen.
+* Going to try an treat the update loop as an event stream to fold over... where the state is a some kind of *game state object*.
+* I want to experiment with rendering to a screen buffer which will be some kind of `pixel [,]` and then draw that to the screen using the spritebatch. I want to try and do this with/in a `MailboxProcessor` (F# in-proc agent).
---
+++ src/LowRezJam/Game.fs
@@ -0,0 +1,37 @@
+﻿module LowRezJam
+open Microsoft.Xna.Framework
+open Microsoft.Xna.Framework.Graphics
+
+type LowRezGame() as this = 
+  inherit Game()
+  
+  let scale x = x * 20
+  let graphics = new GraphicsDeviceManager(this)
+  let mutable spritebatch = Unchecked.defaultof<_>
+  let mutable pixel = Unchecked.defaultof<_>
+  let rect = new Rectangle(x = scale 10, y = scale 10, width = scale 1, height = scale 1)
+
+  do    
+    graphics.PreferredBackBufferWidth <- scale 32
+    graphics.PreferredBackBufferHeight <- scale 32
+    graphics.GraphicsProfile <- GraphicsProfile.HiDef
+    graphics.IsFullScreen <- false
+
+  override this.LoadContent() = 
+    spritebatch <- new SpriteBatch(graphics.GraphicsDevice)
+    pixel <- this.Content.Load<Texture2D>("pixel")
+
+  override this.Update(gametime) = 
+    ()
+    
+  override this.Draw(gametime) = 
+    graphics.GraphicsDevice.Clear(Color.White)
+    spritebatch.Begin()
+    spritebatch.Draw(pixel, rect, Color.GreenYellow)
+    spritebatch.End()
+
+[<EntryPoint>]
+let main _ =
+  let game = new LowRezGame()
+  game.Run()
+  0 // return the magic exit code!
