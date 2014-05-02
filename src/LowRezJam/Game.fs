9199a79de63d3f9595ae704efa7ef77981d3db0b
Matt Ball <saxonmatt@gmail.com>
2014-05-02 15:51:01 +0100

    Publishing an event stream from the update loop.

--- src/LowRezJam/Game.fs
+++ src/LowRezJam/Game.fs
@@ -10,6 +10,7 @@ type LowRezGame() as this =
   let mutable spritebatch = Unchecked.defaultof<_>
   let mutable pixel = Unchecked.defaultof<_>
   let rect = new Rectangle(x = scale 10, y = scale 10, width = scale 1, height = scale 1)
+  let updateEvent = new Event<GameTime>()
 
   do    
     graphics.PreferredBackBufferWidth <- scale 32
@@ -17,21 +18,17 @@ type LowRezGame() as this =
     graphics.GraphicsProfile <- GraphicsProfile.HiDef
     graphics.IsFullScreen <- false
 
+  member this.Updating = updateEvent.Publish
+
   override this.LoadContent() = 
     spritebatch <- new SpriteBatch(graphics.GraphicsDevice)
     pixel <- this.Content.Load<Texture2D>("pixel")
 
   override this.Update(gametime) = 
-    ()
+    updateEvent.Trigger gametime
     
   override this.Draw(gametime) = 
     graphics.GraphicsDevice.Clear(Color.White)
     spritebatch.Begin()
     spritebatch.Draw(pixel, rect, Color.GreenYellow)
     spritebatch.End()
-
-[<EntryPoint>]
-let main _ =
-  let game = new LowRezGame()
-  game.Run()
-  0 // return the magic exit code!
---
+++ src/LowRezJam/Program.fs
@@ -0,0 +1,8 @@
+﻿module Program
+open LowRezJam
+
+[<EntryPoint>]
+let main _ =
+  let game = new LowRezGame()
+  game.Run()
+  0 // return the magic exit code!
